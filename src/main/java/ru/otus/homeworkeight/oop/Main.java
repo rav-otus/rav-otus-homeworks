80a5011f958a79c3958da1786b391d2e9cc6aff5
rav <aarrbb84@mail.ru>
2025-03-01 16:44:52 +0300

    Revision №1

--- src/main/java/ru/otus/homeworkeight/oop/Main.java
+++ src/main/java/ru/otus/homeworkeight/oop/Main.java
@@ -1,12 +1,14 @@
 package ru.otus.homeworkeight.oop;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         String[][] arr = new String[4][4];
         int sumarr = 0;
         arr[0] = new String[]{"1", "5", "3", "1"};
         arr[1] = new String[]{"12", "4", "9", "5"};
-        arr[2] = new String[]{"1", "2", "30", "47"};
+        arr[2] = new String[]{"1", "-2", "30", "47"};
         arr[3] = new String[]{"10", "25", "35", "50"};
 
         try {
@@ -32,8 +34,15 @@ public class Main {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == null){
+                    throw new AppArrayDataException("В ячейке лежит Null");
+                }
                 char[] chararray = array[i][j].toCharArray();
-                for (int k = 0; k < chararray.length; k++) {
+                //if ((chararray[0] != 45 && ((int) chararray[0] < 48 || (int) chararray[0] > 57)) || (chararray[0] == '-' && chararray.length==1))
+                if ((chararray[0] != '-' && ((int) chararray[0] < 48 || (int) chararray[0] > 57)) || (chararray[0] == '-' && chararray.length==1)) {
+                    throw new AppArrayDataException("В строке " + i + " столбце " + j + " лежит не число");
+                }
+                for (int k = 1; k < chararray.length; k++) {
                     if ((int) chararray[k] < 48 || (int) chararray[k] > 57) {
                         throw new AppArrayDataException("В строке " + i + " столбце " + j + " лежит не число");
                     }
