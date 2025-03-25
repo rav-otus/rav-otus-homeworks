ac25451ebf985696d5c9b202e4c5ed170feb8020
rav <aarrbb84@mail.ru>
2025-03-25 23:19:45 +0300

    revision 2

--- src/main/java/ru/otus/homeworkthirteen/AppClient.java
+++ src/main/java/ru/otus/homeworkthirteen/AppClient.java
@@ -8,12 +8,16 @@ public class AppClient {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         while(true) {
-            try (Socket socket = new Socket("localhost", 8080);) {
+            try (Socket socket = new Socket("localhost", 8080)) {
                 Client client = new Client(socket.getInputStream(), socket.getOutputStream());
                 String fromServer = client.getInput().readUTF();
                 System.out.println(fromServer);
                 System.out.println("Введите два числа и операцию");
                 String operationString = scanner.nextLine();
+                if(operationString.equalsIgnoreCase("exit")){
+                    client.send(operationString);
+                    break;
+                }
                 client.send(operationString);
             } catch (IOException e) {
                 throw new RuntimeException(e);
--- src/main/java/ru/otus/homeworkthirteen/AppServer.java
+++ src/main/java/ru/otus/homeworkthirteen/AppServer.java
@@ -10,68 +10,70 @@ public class AppServer {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(8080);
 
-        while(true){
+        while (true) {
             Socket client = server.accept();
             DataInputStream input = new DataInputStream(client.getInputStream());
             DataOutputStream output = new DataOutputStream(client.getOutputStream());
 
             output.writeUTF("Список доступных операций: +, -, *, /");
             String clientInput = input.readUTF();
+            if (clientInput.equalsIgnoreCase("exit")) {
+                client.close();
+                System.out.println("Клиент отключился");
+                continue;
+            }
             String result = calculate(clientInput.toString());
             output.writeUTF(result);
+            output.flush();
         }
     }
 
-    public static String calculate(String str){
+    public static String calculate(String str) {
         String operand1 = "";
         String operand2 = "";
         float result;
         char[] array = str.toCharArray();
-        int i=0;
-        while(((int)array[i])>=48 && ((int)array[i])<=57 || i==array.length-1){
+        int i = 0;
+
+        while (((int) array[i]) >= 48 && ((int) array[i]) <= 57) {
             operand1 = operand1 + array[i];
             i++;
+            if (i == array.length) {
+                break;
+            }
+        }
+        if (operand1.length() == array.length) {
+            return "Отсутствует операция и второй операнд";
         }
         char operation = array[i];
-        if(operand1.length()==0){
+        if (operand1.isEmpty()) {
             return "Первый операнд должен быть числом";
         }
-        //System.out.println(" "+ operand1+ " "+ operation+ " "+(int)operation);
-        //if(i==array.length-1){
-        //    return;
-        //}
-        if(((int)operation) != 42 && ((int)operation) != 43 && ((int)operation) != 45 && ((int)operation) != 47)
-        {
+        if (((int) operation) != 42 && ((int) operation) != 43 && ((int) operation) != 45 && ((int) operation) != 47) {
             return "Операция некорректна";
         }
-        System.out.println(" "+i);
         i++;
-        System.out.println(" "+i);
-        while(i<array.length){
-            if(((int)array[i])<48 && ((int)array[i])>57){
-                //break;
+        while (i < array.length) {
+            if (((int) array[i]) < 48 || ((int) array[i]) > 57) {
                 return "Второй операнд должен быть числом";
             }
-            operand2 +=  array[i];
+            operand2 += array[i];
             i++;
-            System.out.println(" "+i);
         }
-        System.out.println(" "+i);
-        System.out.println(" "+ operand1+ " "+ operation+" "+ operand2);
 
-        if(i!=array.length){
+        if (i != array.length) {
             return "Второй операнд должен быть числом";
         }
-        float oper1 = Float.valueOf(operand1);
-        float oper2 = Float.valueOf(operand2);
+        float oper1 = Float.parseFloat(operand1);
+        float oper2 = Float.parseFloat(operand2);
 
-        if(operation=='+'){
+        if (operation == '+') {
             result = oper1 + oper2;
-        } else if (operation=='-') {
+        } else if (operation == '-') {
             result = oper1 - oper2;
-        } else if (operation=='*') {
+        } else if (operation == '*') {
             result = oper1 * oper2;
-        } else{
+        } else {
             result = oper1 / oper2;
         }
 
